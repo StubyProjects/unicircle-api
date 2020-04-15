1b3f6d5023b4c30477fbfa7d281a78d5992456b4
StubyProjects <jakob.stuby@web.de>
2020-04-15 15:09:41 +0200

    delete productrepo folder

--- src/repositories/products.repository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Like, Repository } from 'typeorm';
-import { Product } from '../entities/product.entity';
-import { GetProductsFilterDto } from '../product/dto/get-products-filter.dto';
-
-@EntityRepository(Product)
-export class ProductsRepository extends Repository<Product> {
-
-}
