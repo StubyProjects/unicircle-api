0fc73896aab3b95a72448d6ddd61f648c2cd9ddf
jakob <jakob.stuby@web.de>
2020-04-29 18:50:38 +0200

    restructered product creation flow

--- src/product/repositories/productlisting.repository.ts
+++ src/product/repositories/productlisting.repository.ts
@@ -1,23 +1,23 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Productlisting } from '../entities/productlisting.entity';
-import {CreateProductInput} from "../dto/create-product.input";
 import { Product } from '../entities/product.entity';
+import { CreateListingInput } from '../dto/create-listing.input';
 
 @EntityRepository(Productlisting)
 export class ProductlistingRepository extends Repository<Productlisting> {
     /**
      * Creates a new product listing in the database.
-     * @param createProductInput - attributes of the product which are needed to create the listing
+     * @param createListingInput
+     * @param productId
      * @param user - seller who makes the listing
-     * @param existingProduct
      * @param newCondition - the condition in which the product is in
      */
-    async createProductListing(createProductInput: CreateProductInput, existingProduct: Product, newCondition, user) {
-        const { price } = createProductInput;
+    async createProductListing(createListingInput: CreateListingInput, productId: Product, newCondition, user) {
+        const { price } = createListingInput;
         const productListing = new Productlisting();
         productListing.createdAt = new Date(Date.now()).toLocaleString();
         productListing.price = price;
-        productListing.product = existingProduct;
+        productListing.product = productId;
         productListing.condition = newCondition;
         productListing.userId = user.sub;
         await this.save(productListing);
