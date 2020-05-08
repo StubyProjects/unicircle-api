dc3f0f70b7054e07a9e6bcb5c20b5e65bcf9b65a
PaulDiTi <st161607@stud.uni-stuttgart.de>
2020-05-08 13:04:44 +0200

    user entity fixed

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,4 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { IsString } from 'class-validator';
 
 @Entity()
@@ -8,11 +8,14 @@ export class UserEntity extends BaseEntity {
   id: string;
 
   @IsString()
+  @Column()
   auth0Id: string;
 
   @IsString()
+  @Column()
   mangoPayId: string;
 
   @IsString()
+  @Column()
   routeId: string;
 }
